8aabab9b042ad7604785fe0b8d7f032633315762
Marina-del-rio <marinadelriosanvicens@gmail.com>
2025-10-10 11:01:42 +0200

    ejecutar comando

--- src/Main.java
+++ src/Main.java
@@ -1,8 +1,12 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 import java.util.Scanner;
 
 public class Main {
+    private static Scanner sc = new Scanner(System.in);
     public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
+
         int option = 0;
 
         while(option!=5){
@@ -29,13 +33,32 @@ public class Main {
                     System.out.println("Opción no válida");
                 }
             }
-
         }
-
     }
     private static void ejecutarComando() {
+        System.out.println("Introduce un comando a ejecutar");
+        String comando = sc.nextLine();
+        try {
+            ProcessBuilder pb = new ProcessBuilder(comando);
+            Process process = pb.start();
 
+            BufferedReader reader = new BufferedReader(new InputStreamReader(process.getInputStream()));
+            String line;
+            while ((line = reader.readLine()) != null) {
+                System.out.println(line);
+            }
+
+            int exitCode = process.waitFor();
+            System.out.println("El proceso ha finalizado" + exitCode);
+
+        } catch (IOException e) {
+            System.out.println("Error al ejecutar el comando: " + e.getMessage());
+        }catch (InterruptedException e) {
+        System.out.println("La espera fue interrumpida: " + e.getMessage());
+        Thread.currentThread().interrupt();
+        }
     }
+
     private static void lanzarAplicacion() {
     }
 
@@ -46,8 +69,20 @@ public class Main {
 
     }
 
-
-    //Menu de la aplicacion
+    /**
+     * Muestra el menú principal de la aplicación de gestión de procesos.
+     * <p>
+     * Este menú ofrece al usuario las diferentes opciones disponibles:
+     * <ul>
+     *     <li>1. Ejecutar comando</li>
+     *     <li>2. Lanzar aplicación</li>
+     *     <li>3. Ejecutar comando con redirección</li>
+     *     <li>4. Ejecutar comando con variables de entorno</li>
+     *     <li>5. Salir</li>
+     * </ul>
+     *
+     * @return una cadena de texto con el menú formateado que se mostrará al usuario.
+     */
     private static String menu(){
         return "\n=== Gestión de Procesos con ProcessBuilder === \n" +
                 "1. Ejecutar comando\n"+
